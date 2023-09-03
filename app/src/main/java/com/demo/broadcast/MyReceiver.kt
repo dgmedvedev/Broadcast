9aa67de699d1741810407b788015e0f55d92f628
dgmedvedev <dg-medvedev@mail.ru>
2023-09-03 16:39:44 +0300

    Created a custom receiver, added several actions

--- app/src/main/java/com/demo/broadcast/MyReceiver.kt
+++ app/src/main/java/com/demo/broadcast/MyReceiver.kt
@@ -8,9 +8,27 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked, count = $count", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> { // слушаем изменение состояния режима в полете
-                Toast.makeText(context, "Airplane mode change", Toast.LENGTH_SHORT).show()
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context,
+                    "Airplane mode change. Turned on: $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            Intent.ACTION_BATTERY_LOW -> { // малый уровень батареи
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    companion object {
+
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
